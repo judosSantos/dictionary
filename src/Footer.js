9476ccc9e2a1c199391563a18547631eea4b21b5
judossantos13 <judossantos13@gmail.com>
2026-08-23 15:56:26 +0200

    upadated css in general

--- src/Footer.js
+++ src/Footer.js
@@ -3,12 +3,15 @@ import "./Footer.css";
 
 export default function Footer() {
   return (
-    <div className="container row">
-      <div className="col-6">
-        <p>© 2026 JS.</p>
-      </div>
-
-      <div className="col-6">GitHub</div>
+    <div className="container row footer-container">
+      <p>Designed & developed by Juliana Santos</p>
+      <p>Built with React · JavaScript · Axios · SheCodes API.</p>
+      <p>
+        Open-source on{" "}
+        <a href="https://github.com/judosSantos/dictionary">GitHub</a> · Hosted
+        on{" "}
+        <a href="https://app.netlify.com/teams/judossantos/projects">Netlify</a>
+      </p>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,5 @@
 import React from "react";
 import logo from "./logo.png";
-import "./Header.css";
 
 export default function Header() {
   return (
