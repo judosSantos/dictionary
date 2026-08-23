8ea02e71bc73c4addf2c27dfe6e41e729f095d40
judossantos13 <judossantos13@gmail.com>
2026-08-23 13:34:24 +0200

    updatet footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,8 +4,8 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <div className="container row">
-      <div className="col-5">
-        <p>© 2026 Lexicon Lexicographical Institute.</p>
+      <div className="col-6">
+        <p>© 2026 JS.</p>
       </div>
 
       <div className="col-6">GitHub</div>
