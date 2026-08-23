6dd8e998ceece24ae352058982c1a5abe33b7095
judossantos13 <judossantos13@gmail.com>
2026-08-23 13:17:32 +0200

    updated css

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,13 +5,16 @@ export default function Meaning(props) {
     <div className="meanings">
       {props.meanings.map((meaning, index) => (
         <div className="meaning-definition" key={index}>
-          <div className="">
+          <div className="meaning-title">
             <strong>{meaning.partOfSpeech}</strong>
           </div>
-
           <p className="meaning-definition">{meaning.definition}</p>
-
-          {meaning.example && <p className="example">"{meaning.example}"</p>}
+          {meaning.example && (
+            <p className="exemple">exemple: {meaning.example}"</p>
+          )}
+          {meaning.synonyms && (
+            <p className="synonym"> synonym: {meaning.synonyms}</p>
+          )}
         </div>
       ))}
     </div>
--- src/searchResults.js
+++ src/searchResults.js
@@ -31,49 +31,6 @@ export default function SearchResults(props) {
             </div>
           </div>
 
-          <div className="result-card ">
-            <div className="card-title">
-              <span>
-                <img
-                  src={folder}
-                  alt="Folder"
-                  width="30"
-                  className="d-inline-block align-text-top"
-                />
-              </span>
-              <strong>Adjective</strong>
-            </div>
-
-            <p>
-              Lasting for a very short time. Something that is fleeting,
-              transitory, or short-lived.
-            </p>
-          </div>
-
-          <div className="result-card">
-            <div className="card-title">
-              <span>
-                <img
-                  src={folder}
-                  alt="Folder"
-                  width="30"
-                  className="d-inline-block align-text-top"
-                />
-              </span>
-              <strong>Synonyms</strong>
-            </div>
-
-            <div className="synonyms">
-              <span>fleeting</span>
-              <span>transitory</span>
-              <span>brief</span>
-              <span>short-lived</span>
-              <span>momentary</span>
-              <span>passing</span>
-              <span>temporary</span>
-            </div>
-          </div>
-
           <div className="result-card">
             <div className="card-title">
               <span>
